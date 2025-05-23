f21aac3d9f7286967fdc0ccd4951b146444689b5
Anna Tamakulova <annatamakulova@yandex.ru>
2025-05-23 21:20:10 +0300

    add GetNotification

--- src/LabTracker/LabTracker.Notifications/Services/NotificationService.cs
+++ src/LabTracker/LabTracker.Notifications/Services/NotificationService.cs
@@ -67,13 +67,13 @@ public class NotificationService : INotificationService
             await _notificationRepository.MarkAsReadAsync(userId, notificationIds);
         }
     }
-
+ 
     public async Task<Notification> GetNotificationAsync(Guid notificationId) 
     {
         var notification = await _notificationRepository.GetByIdAsync(notificationId);
-        if (notification == null)
+        if (notification is null)
             return null;
         
         return notification;
     }
-}
+}  
