9e8de1a4261cbd68593a73fe9a06d1ea88e6a1f7
mdvdev <nikita2102antonenkov@gmail.com>
2025-05-28 16:46:18 +0300

    Add oligarch endpoint

--- src/LabTracker/LabTracker.CourseMembers/Web/CourseStudentController.cs
+++ src/LabTracker/LabTracker.CourseMembers/Web/CourseStudentController.cs
@@ -2,6 +2,9 @@ using LabTracker.CourseMembers.Abstractions.Services;
 using LabTracker.CourseMembers.Domain;
 using LabTracker.CourseMembers.Web.Dtos;
 using LabTracker.Courses.Abstractions.Services;
+using LabTracker.Courses.Domain;
+using LabTracker.Submissions.Abstractions.Repositories;
+using LabTracker.Submissions.Domain;
 using LabTracker.User.Abstractions.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,15 +18,18 @@ public class CourseStudentController : ControllerBase
     private readonly ICourseService _courseService;
     private readonly ICourseMemberService _courseMemberService;
     private readonly IUserRepository _userRepository;
+    private readonly IOligarchStudentRepository _oligarchStudentRepository;
 
     public CourseStudentController(
         ICourseService courseService,
         ICourseMemberService courseMemberService,
-        IUserRepository userRepository)
+        IUserRepository userRepository,
+        IOligarchStudentRepository oligarchStudentRepository)
     {
         _courseService = courseService;
         _courseMemberService = courseMemberService;
         _userRepository = userRepository;
+        _oligarchStudentRepository = oligarchStudentRepository;
     }
 
     [HttpGet]
@@ -57,6 +63,38 @@ public class CourseStudentController : ControllerBase
 
         return Ok();
     }
+
+    [HttpPatch("{studentId}")]
+    [Authorize(Policy = "TeacherAndCourseMember")]
+    public async Task<IActionResult> UpdateStudentStatus(Guid courseId, Guid studentId, [FromQuery] bool isOligarch)
+    {
+        var user = await _userRepository.GetByIdAsync(studentId);
+        if (user is null)
+            return NotFound($"User with ID '{studentId}' not found.");
+
+        if (!user.IsStudent)
+            return BadRequest($"User with ID '{studentId}' is not a student.");
+
+        var course = await _courseService.GetCourseDetailsAsync(courseId);
+        if (course is null)
+            return NotFound($"Course with ID '{courseId}' not found.");
+
+        var courseMember =
+            await _courseMemberService.GetCourseMemberDetailsAsync(new CourseMemberKey(courseId, studentId));
+        if (courseMember is null)
+            return NotFound($"Course member with ID '{studentId}' not found.");
+
+        if (course.QueueMode != QueueMode.Oligarchic)
+            return BadRequest($"Course with ID '{courseId}' is not in oligarchic mode.");
+
+        if (isOligarch)
+            await _oligarchStudentRepository.CreateAsync(OligarchStudent.CreateNew(studentId, courseId));
+        else
+            await _oligarchStudentRepository.DeleteAsync(studentId, courseId);
+
+        return Ok();
+    }
+
 
     [HttpDelete("{studentId}")]
     [Authorize(Policy = "TeacherAndCourseMember")]
